da57e19f123ee89c83b47866e564c7f31ebec2aa
richrein <rich.rein@datastax.com>
2016-04-12 18:26:45 -0700

    get_Host.sh - added.  get_GroupPwd.sh - refined

--- get_GroupPwd.sh
+++ get_GroupPwd.sh
@@ -1,10 +1,13 @@
 
 #
-set -x
+# Display the secret require to setup a VPN
+#
+
+# set -x   # show script execution in verbose mode
+
 if [ x"$1" == x ]; then echo missing pcf file name; exit -1 ; fi
 pcf_file="$1" ; shift
 enc_GroupPwd=$(grep enc_GroupPwd "$pcf_file" | sed -e 's/^[^=]*=\([0-9A-Za-z]*\).*/\1/' )
-enc_GroupPwd=$(echo $enc_GroupPwd)
-echo "$enc_GroupPwd"
-./cisco-decrypt $enc_GroupPwd
+GroupPwd=$(./cisco-decrypt $enc_GroupPwd)
+echo "$GroupPwd"
 
